03f1f8a0382f47261ea785456394784b34d32cdb
SinekaRamu <sineka2001cud@gmail.com>
2024-02-29 20:20:26 +0530

    makefile for jquery

--- make-file.sh
+++ make-file.sh
@@ -25,14 +25,6 @@ echo "<!DOCTYPE html>
 </html>
 " >> index.html
 touch style.css
-echo "@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Poppins:wght@500&display=swap");
-
-* {
-  margin: 0;
-  padding: 0;
-  font-family: "poppins", sans-serif;
-  box-sizing: border-box;
-}" >> style.css
 touch script.js
 
 cd ..
